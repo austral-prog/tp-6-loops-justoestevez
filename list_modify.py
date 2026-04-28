42193ccefedfa8fab1d178267c098bdf5051e9b0
justoestevez <jeestevez@mail.austral.edu.ar>
2026-04-28 11:57:42 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -13,10 +13,11 @@ def put(value, lst):
         # colors ahora es ["Red", "Blue", "Green"]
     """
     for i in range(len(lst)):
-        if lst(i) == "":
-            lst(i) = value
-            return i
-    return -1
+        if lst[i] == "": # Si encontré un lugar vacío
+            lst[i] = value # Lo ocupo con el nuevo valor
+            return i # Devuelvo la posición donde lo puse y CORTO la función
+            
+    return -1 # Si terminó el for y no había ningún ""
 
 def remove(value, lst):
     """
@@ -29,9 +30,12 @@ def remove(value, lst):
         remove("Red", colors) -> 2
         # colors ahora es ["", "Green", "", "Blue"]
     """
-    count = 0
+   
+    contador_borrados = 0
+    
     for i in range(len(lst)):
-        if lst(i) == value:
-            lst(i) = ""
-            count = count + 1
-    return count
+        if lst[i] == value:
+            lst[i] = "" # Lo borramos dejando el string vacío
+            contador_borrados = contador_borrados + 1
+            
+    return contador_borrados # Devolvemos el total de veces que lo borramos
